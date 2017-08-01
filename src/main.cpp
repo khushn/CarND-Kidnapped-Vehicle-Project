fa6f319d82fe4774056088fc243458d3020aa900
khush <khushnood.naqvi@gmail.com>
2017-08-01 14:58:06 +0530

    I added two lines, which are supposed to be fix for the simulator. Now it works.

--- src/main.cpp
+++ src/main.cpp
@@ -145,7 +145,11 @@ int main()
           json msgJson;
           msgJson["best_particle_x"] = best_particle->x;
           msgJson["best_particle_y"] = best_particle->y;
-          msgJson["best_particle_theta"] = best_particle->theta;
+          double theta = best_particle->theta;
+          if (theta < 0.) {
+            theta += 2. * M_PI;
+          }
+          msgJson["best_particle_theta"] = theta;
 
           //Optional message data used for debugging particle's sensing and associations
           string assoc_str =  pf.getAssociations(*best_particle);          
